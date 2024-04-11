4a689d39dd488a5e5111c09425fcec97fab6defe
denixx-dev <denissm2002@yandex.ru>
2024-04-11 22:30:20 +0300

    added language select activity

---
+++ app/src/main/java/org/kazino/language_app/LanguageSelect.java
@@ -0,0 +1,14 @@
+package org.kazino.language_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LanguageSelect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_language_select);
+    }
+}
--- app/src/main/java/org/kazino/language_app/Onboarding_3.java
+++ app/src/main/java/org/kazino/language_app/Onboarding_3.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Onboarding_3 extends AppCompatActivity {
@@ -19,6 +20,14 @@ public class Onboarding_3 extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(Onboarding_3.this, MainActivity.class));
+            }
+        });
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Onboarding_3.this, LanguageSelect.class));
             }
         });
     }
