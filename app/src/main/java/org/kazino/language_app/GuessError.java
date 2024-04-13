17c8ec7d158e3f52841102cbf61f62abb21eae7d
denixx-dev <denissm2002@yandex.ru>
2024-04-14 00:39:48 +0300

    done guess the animal and word practice activities

---
+++ app/src/main/java/org/kazino/language_app/GuessError.java
@@ -0,0 +1,34 @@
+package org.kazino.language_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class GuessError extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guess_error);
+
+        Button nextButton = findViewById(R.id.nextButton);
+        Button tryAgainButton = findViewById(R.id.tryAgainButton);
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(GuessError.this, MainActivity.class));
+            }
+        });
+
+        tryAgainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(GuessError.this, GuessTheAnimal.class));
+            }
+        });
+    }
+}
